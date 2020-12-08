76ffaab1cfa69e32ba57ef268473cda755d139c6
Nick On <nick.on437@gmail.com>
2020-12-09 00:58:24 +1100

    🎨 Style - Integrate bootstrap and construct basic layout

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container className='py-3 text-center'>
+        Copyright &copy; Shopee
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
