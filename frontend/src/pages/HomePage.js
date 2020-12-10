40ba22919b4ed4ca482df2e26e511f7fc6288480
Nick On <nick.on437@gmail.com>
2020-12-10 13:16:19 +1100

    ⚡ Improve - Fetch backend to frontend

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,26 @@
+import { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import products from '../products';
 import Product from '../components/Product';
+import axios from 'axios';
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
           </Col>
         ))}
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Image, Row, Col, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
-import products from '../products';
+import {
+  Image,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from 'react-bootstrap';
 import Rating from '../components/Rating';
+import axios from 'axios';
 
 const ProductPage = ({ match }) => {
-  const product = products.find((item) => item._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      console.log('id', match.params.id);
+      const { data } = await axios.get(`/api/product/${match.params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, []);
 
   return (
     <div>
@@ -30,14 +47,16 @@ const ProductPage = ({ match }) => {
         </Col>
         <Col>
           <ListGroup>
-            <ListGroupItem>
-              Price: ${product.price}
-            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
             <ListGroupItem>
               Status: {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
             </ListGroupItem>
             <ListGroupItem>
-              <Button type='button' className='btn-block' disabled={product.countInStock <= 0}>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={product.countInStock <= 0}
+              >
                 Add to Cart
               </Button>
             </ListGroupItem>
