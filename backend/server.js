8567b81d1c2289647ddc042dec1a9542718140f0
Nick On <nick.on437@gmail.com>
2020-12-10 15:13:53 +1100

    ⚡ Improve - Add dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const products = require('./data/products');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,5 @@ app.get('/api/product/:id', (req, res) => {
   res.json(product);
 })
 
-app.listen(5000, console.log('Server is running on port 5000'));
+const PORT = process.env.PORT ?? 5000;
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
