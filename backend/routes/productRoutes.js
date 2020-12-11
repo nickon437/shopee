30678fa1050d71d16030de15fdc07b9f9ab598b9
Nick On <nick.on437@gmail.com>
2020-12-11 21:23:59 +1100

    ⚡ Improve - Fetch data from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import asyncHanlder from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+const router = express.Router();
+
+/**
+ * @description Fetch all products
+ * @route       GET /api/products
+ * @access      Public
+ */
+router.get(
+  '/',
+  asyncHanlder(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+/**
+ * @description Fetch a product
+ * @route       GET /api/product/:id
+ * @access      Public
+ */
+router.get(
+  '/:id',
+  asyncHanlder(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status('404').json({ message: 'Product not found' });
+    }
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/connectDB.js';
 import 'colors';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -13,6 +13,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT ?? 5000;
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
