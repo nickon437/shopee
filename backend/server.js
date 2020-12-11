4a6c14fd85b215a8cc29bcf07db7da12d715615c
Nick On <nick.on437@gmail.com>
2020-12-11 19:26:48 +1100

    ✨ New - Integratemongodb with backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const products = require('./data/products');
-const dotenv = require('dotenv');
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+import connectDB from './config/connectDB.js';
+import 'colors';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -10,14 +14,5 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find((item) => item._id === req.params.id);
-  res.json(product);
-})
-
 const PORT = process.env.PORT ?? 5000;
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
