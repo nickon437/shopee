dd276df9b9c8babffdcf044890d643032c0b4657
Nick On <nick.on437@gmail.com>
2020-12-15 11:26:20 +1100

    ✨ New - Add add to cart functionality

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
 
-const CartPage = () => {
+const CartPage = ({ match, location }) => {
+  const dispatch = useDispatch();
+  const productId = match.params.id;
+  const qty = Number(location.search?.split('=')[1] ?? 1); 
+
+  useEffect(() => {
+    dispatch(addToCart(productId, qty));
+  }, [dispatch, productId, qty]);
+
+
+
   return (
     <div>
       Cart
