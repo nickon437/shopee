27dad0bd8b88cfeeb1bceddfb1987a55bd0518a0
Nick On <nick.on437@gmail.com>
2021-01-01 00:13:32 +1100

    ✨ New - Add createProduct to backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -44,4 +44,39 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+/**
+ * @description Create a product
+ * @route       POST /api/product/:id
+ * @access      Private / Admin
+ */
+const createProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    image,
+    description,
+    brand,
+    category,
+    price,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.create({
+    name,
+    image,
+    description,
+    brand,
+    category,
+    price,
+    countInStock,
+  });
+
+  if (product) {
+    res.status(201)
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Fail to create new product');
+  }
+});
+
+export { getProducts, getProductById, deleteProduct, createProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,13 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
