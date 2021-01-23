e62e599b52e3ac6d9d7b02e871c7b178e74a2ac4
Nick On <nick.on437@gmail.com>
2021-01-23 13:24:36 +1100

    ✨ New - Add image upload config and endpoint

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const router = express.Router;
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const checkFileType = (file, cb) => {
+  const fileTypes = /jpg|jpeg|png/;
+  const isExtMatched = fileTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const isMimeTypeMatched = fileTypes.test(file.mimetype);
+
+  if (isExtMatched && isMimeTypeMatched) {
+    return cb(null, true);
+  } else {
+    return cb('Images only!');
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => checkFileType(file, cb),
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
